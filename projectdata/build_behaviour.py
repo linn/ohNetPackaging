874bd077a76dcd51f187b3f7893a043194c4046a
Iain McLeod <iain.mcleod@linn.co.uk>
2015-05-07 11:45:14 +0100

    updated packaging task for iOs-arm-universal

--- projectdata/build_behaviour.py
+++ projectdata/build_behaviour.py
@@ -4,6 +4,7 @@ import sys
 import os
 import shutil
 import glob
+import subprocess
 
 try:
     from ci import (OpenHomeBuilder, require_version)
@@ -16,17 +17,26 @@ require_version(45)
 
 class Builder(OpenHomeBuilder):
     output_dir = "build/packages"
+    build_dir = "build"
 
     def setup(self):
         self.nuget_server = self.env.get('NUGET_SERVER')
         self.nuget_api_key = self.env.get('NUGET_API_KEY')
 
     def clean(self):
-        if os.path.isdir(self.output_dir):
-            shutil.rmtree(self.output_dir)
+        if os.path.isdir(self.build_dir):
+            shutil.rmtree(self.build_dir)
 
     def build(self):
+        os.makedirs(self.build_dir)
         os.makedirs(self.output_dir)
+        iosFatBinaryDir = os.path.join(self.build_dir, 'ios/arm-universal/ohNet/lib')
+        os.makedirs(iosFatBinaryDir)
+        architectures = ['armv7', 'arm64']
+        archives = str.join(" ", ["./dependencies/ios/%s/ohNet/lib/libohNetCore.a" % (a) for a in architectures])
+        libtoolcmd = "/usr/bin/libtool -static %s -o \"%s/libohNetCore.a\"" % (archives, iosFatBinaryDir)
+        print libtoolcmd
+        subprocess.check_call(libtoolcmd, cwd=os.getcwd(), shell=True)
         self.pack_nuget('src/ohNet.nuspec', '.')
         self.pack_nuget('src/ohNetGeneratedProxies.nuspec', '.')
         self.pack_nuget('src/ohNetGeneratedProviders.nuspec', '.')
