44a5c977cba43adc7ffd7a9ff45a3be7b2d01dcb
Ewan <git@ewan.emccartney.co.uk>
2022-11-08 14:24:40 +0000

    Convert to make use of new ohDevTools versions.

--- projectdata/build_behaviour.py
+++ projectdata/build_behaviour.py
@@ -15,9 +15,45 @@ except ImportError:
 require_version(67)
 
 
+
 class Builder(OpenHomeBuilder):
     output_dir = "build/packages"
     build_dir = "build"
+
+    def pack_nuget(self, project_name, base_path='', props=None, output_path='build/packages', include_references=True):
+        '''
+        Creates a nuget package based on the supplied project file
+        '''
+        props = {} if props is None else props
+        props['Configuration'] = self.configuration
+        props['version'] = self.version if self.version is not None else '0.0.0.1-dev'
+        props_str = ';'.join(['%s=%s' % (k, v) for (k, v) in props.items()])
+
+        args = ['./nuget/nuget.exe', 'pack', project_name, '-BasePath', base_path, '-Properties', props_str]
+        if output_path is not None:
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+            args += ['-OutputDirectory', output_path]
+        if include_references:
+            args += ['-IncludeReferencedProjects']
+        self.cli(args)
+
+    def publish_nuget(self, package, api_key=None, server=None, config_file='nuget.config'):
+        '''
+        Publishes a nuget package to a specified NuGet server.
+        '''
+        args = ['./nuget/nuget.exe', 'push', package]
+
+        #nuget can be slow, so set a long timeout (in secs)
+        args += ['-Timeout', "10000"]
+
+        if api_key is not None:
+            args += [api_key]
+        if server is not None:
+            args += ['-Source', server]
+        if config_file is not None and os.path.isfile(config_file):
+            args += ['-ConfigFile', config_file]
+        self.cli(args)
 
     def setup(self):
         self.nuget_api_key = self.env.get('NUGET_API_KEY')
