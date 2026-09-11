3816e63bb22ea6673f02ce3bce927de6c2ae535b
Ewan <git@ewan.emccartney.co.uk>
2026-09-11 14:40:43 +0100

    Build: Ewan needs to learn python.

--- jenkinsBuild.py
+++ jenkinsBuild.py
@@ -116,6 +116,6 @@ if __name__ == '__main__':
     print(f'    Nuget API Key: present')
     print('-------')
 
-    chdir('ohNetPackaging')
+    os.chdir('ohNetPackaging')
 
     b = Runner(nuget_api_key, ohnet_version, ohnet_generated_version, release_version, publish_release)
